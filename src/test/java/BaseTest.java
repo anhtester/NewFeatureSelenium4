1b7187effc478d25954b5b5128df421bceff0f52
Anh Tester <thaian.it15@gmail.com>
2022-12-07 20:18:53 +0700

    Add Chrome DevTools features

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void createBrowser() {
+        System.out.println("Start Chrome browser from BaseTest...");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        System.out.println("Close browser from BaseTest...");
+        driver.quit();
+    }
+
+    public static void sleep(double second) {
+        try {
+            Thread.sleep((long) (1000 * second));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/HandleChromeDevTools.java
@@ -0,0 +1,84 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.v108.log.Log;
+import org.openqa.selenium.devtools.v108.network.Network;
+import org.openqa.selenium.devtools.v108.network.model.ConnectionType;
+import org.openqa.selenium.devtools.v108.security.Security;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+public class HandleChromeDevTools extends BaseTest {
+
+    @Test
+    public void emulateSlowNetwork() {
+        DevTools devTools = ((HasDevTools) driver).getDevTools();
+        devTools.createSession();
+        // Enable the Network domain of devtools
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+        // Set 4G network
+        devTools.send(Network.emulateNetworkConditions(
+                false,
+                500,
+                2000000,
+                3000000,
+                Optional.of(ConnectionType.CELLULAR3G)));
+
+        driver.get("https://demo.activeitzone.com/ecommerce/");
+        driver.findElement(By.xpath("//i[@class='la la-close fs-20']")).click();
+        driver.findElement(By.xpath("(//a[normalize-space()='Registration']/parent::li)/preceding-sibling::li/a[normalize-space()='Login']")).click();
+        driver.findElement(By.xpath("//button[@onclick='autoFillCustomer()']")).click();
+        sleep(2);
+    }
+
+    @Test
+    public void emulateGeolocation() {
+        DevTools devTools = ((HasDevTools) driver).getDevTools();
+        devTools.createSession();
+
+//        devTools.send(Emulation.setGeolocationOverride(
+//                Optional.of(52.5043),
+//                Optional.of(13.4501),
+//                Optional.of(1))
+//        );
+
+        driver.get("https://where-am-i.org/");
+
+        System.out.println("-----GeoLocation----");
+        System.out.println("Location:" + driver.findElement(By.id("address")).getText());
+        System.out.println("Latitude:" + driver.findElement(By.id("latitude")).getText());
+        System.out.println("Longitude:" + driver.findElement(By.id("longitude")).getText());
+        System.out.println("--------------------");
+    }
+
+    @Test
+    public void viewConsoleLogs() {
+        DevTools devTools = ((HasDevTools) driver).getDevTools();
+        devTools.createSession();
+        devTools.send(Log.enable());
+
+        devTools.addListener(Log.entryAdded(), logEntry -> {
+            System.out.println("-------------------------------------------");
+            System.out.println("Request ID = " + logEntry.getNetworkRequestId());
+            System.out.println("URL = " + logEntry.getUrl());
+            System.out.println("Source = " + logEntry.getSource());
+            System.out.println("Level = " + logEntry.getLevel());
+            System.out.println("Text = " + logEntry.getText());
+            System.out.println("Timestamp = " + logEntry.getTimestamp());
+            System.out.println("-------------------------------------------");
+        });
+
+        driver.get("https://anhtester.com/abc");
+    }
+
+    @Test
+    public void checkSecuritySite() {
+        DevTools devTools = ((HasDevTools) driver).getDevTools();
+        devTools.createSession();
+        devTools.send(Security.enable());
+        devTools.send(Security.setIgnoreCertificateErrors(true));
+
+        driver.get("https://anhtester.com");
+    }
+}
